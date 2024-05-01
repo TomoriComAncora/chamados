8892f54bc6d08eef2d77a80f69d789965483a0ba
TomoriComAncora <lucas_fernando@ufms.br>
2024-05-01 11:31:24 -0400

    Feat: Tela de cadastro e dashboard

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png"
 
 function SignUp() {
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
   return (
-    <div>
-        <h1>SignUp</h1>
+    <div className="container">
+      <div className="login">
+        <div className="login-area">
+          <img src={logo} alt="logo" />
+        </div>
+        <form>
+          <h1>Nova conta</h1>
+          <input
+            type="text"
+            placeholder="Seu nome"
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+          />
+
+          <input
+            type="text"
+            placeholder="email@email.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <input
+            type="password"
+            placeholder="********"
+            value={senha}
+            onChange={(e) => setSenha(e.target.value)}
+          />
+
+          <button type="submit">Cadastrar</button>
+        </form>
+        <Link to={"/"}>Já possui uma conta? Faça login</Link>
+      </div>
     </div>
-  )
+  );
 }
 
-export default SignUp
+export default SignUp;
